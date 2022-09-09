0d4d1e3588189655422dc27bbfdf774dc5cfda30
Devin Daniels <devingdaniels@gmail.com>
2022-09-09 12:53:01 -0700

    Added linter and prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = "Hello webpackrewrew"
-  
-    return element;
-  }
+  const element = document.createElement('div');
 
-  document.getElementById('anchor').append(component())
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = 'Hello friend';
+
+  return element;
+}
+
+function test() {
+  console.log('test');
+}
+
+document.getElementById('anchor').append(component());
+test();
